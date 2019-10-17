eaaeaff36d77e65f4fd369ac21b565aec454c28b
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:30 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan39-NilaiTerbesarDanTerkecil/src/if1/pkg10118008/latihan39/nilaiterbesardanterkecil/IF110118008Latihan39NilaiTerbesarDanTerkecil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+* @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: menentukan nilai terbesar dan nilai terkecil
+ */
+public class IF110118008Latihan39NilaiTerbesarDanTerkecil {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Nilai nilai = new Nilai();
+		nilai.inputPetugas();
+		nilai.inputNilai();
+		nilai.outputNilai();
+		
+	}
+	
+}
